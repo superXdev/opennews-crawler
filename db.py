a1a86458036619db8c2fe3175e24be52731ef7da
Fikri Rudiansyah <fikrizhanking@gmail.com>
2021-01-06 21:07:07 +0700

    Adding mysql for driver selector configuration

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ import datetime
 from datetime import datetime
 import pytz
 import os
+import mysql.connector
 
 class db():
 	
@@ -18,30 +19,40 @@ class db():
 	def insert(self, title, link, image, tag, source):
 		try:
 		    # Connect to an existing database
-		    connection = psycopg2.connect(user=os.environ.get('DB_USER'),
-		    	password=os.environ.get('DB_PASSWORD'),
-		    	host=os.environ.get('DB_HOST'),
-		    	port="5432",
-		    	database=os.environ.get('DB_NAME'))
 
-		    cursor = connection.cursor()
-		    # Check
-		    insert_query =  f"SELECT EXISTS(SELECT 1 FROM news WHERE link = '{link}');"
-		    cursor.execute(insert_query)
-		    res = cursor.fetchone()
+			if os.environ.get('DB_DRIVER') == 'pgsql':
+				connection = psycopg2.connect(
+					user=os.environ.get('DB_USER'),
+					password=os.environ.get('DB_PASSWORD'),
+					host=os.environ.get('DB_HOST'),
+					port="5432",
+					database=os.environ.get('DB_NAME'))
+			else:
+				connection = mysql.connector.connect(
+					host=os.environ.get('DB_HOST'),
+					user=os.environ.get('DB_USER'),
+					passwd=os.environ.get('DB_PASSWORD'),
+					database=os.environ.get('DB_NAME')
+				)
 
-		    timezone = pytz.timezone('Asia/Jakarta')
-		    now = timezone.localize(datetime.now())
+			cursor = connection.cursor()
+			# Check
+			insert_query =  f"SELECT EXISTS(SELECT 1 FROM news WHERE link = '{link}');"
+			cursor.execute(insert_query)
+			res = cursor.fetchone()
 
-		    if res[0] == False:
-		    	cursor.execute("""INSERT INTO news (title, link, image, tag, source, created_at, updated_at) 
-		    		VALUES (%s, %s, %s, %s, %s, %s, %s)""",
-		    		(title, link, image, tag.title(), source, now, now))
-		    	self.inserted += 1
-		    	connection.commit()
+			timezone = pytz.timezone('Asia/Jakarta')
+			now = timezone.localize(datetime.now())
 
-		except (Exception, psycopg2.Error) as error:
-			pass
+			if res[0] == False:
+				cursor.execute("""INSERT INTO news (title, link, image, tag, source, created_at, updated_at) 
+					VALUES (%s, %s, %s, %s, %s, %s, %s)""",
+					(title, link, image, tag.title(), source, now, now))
+				self.inserted += 1
+				connection.commit()
+
+		except (Exception) as error:
+			print(error)
 		finally:
 			if (connection):
 				cursor.close()
