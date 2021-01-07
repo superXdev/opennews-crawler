234823f7ef4b171c22e23d24ff35679997b4647b
Fikri Rudiansyah <fikrizhanking@gmail.com>
2021-01-07 09:34:19 +0700

    testing

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def start():
 	print(db.get_inserted(), 'news inserted.')
 	db.set_inserted(0)
 
-schedule.every(10).minutes.do(start)
+schedule.every(0).minutes.do(start)
 
 while True:
 	schedule.run_pending()
